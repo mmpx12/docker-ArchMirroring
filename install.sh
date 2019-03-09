13047322c1717f25504ef8da8ecbfbdb15fc0628
mmpx12 <45767624+mmpx12@users.noreply.github.com>
2019-03-09 04:26:01 +0000

    .

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ docker build -t arch-mirroring .
 read -ep "Start the container right now(y|n)? " yn
 
 case "$yn" in
-  Y*|y* ) docker run -d -p 80:80 arch-mirroring
+  Y*|y* ) docker run --restart always -d -p 80:80 arch-mirroring
       ;;
-  N*|n* ) echo "Start container with \"docker run -d -p 80:80 arch-mirroring.\""
+  N*|n* ) echo "Start container with \"docker run --restart always -d -p 80:80 arch-mirroring.\""
       ;;
 esac
